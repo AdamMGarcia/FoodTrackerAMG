163e38588c61ee1db1e0e4e599926eaf012b47bd
Adam Garcia <amg16h@lib-301-02-9110.local>
2019-02-18 08:40:08 -0600

    Initial Commit

---
+++ FoodTrackerAMG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTrackerAMG
+//
+//  Created by Adam Garcia on 2/18/19.
+//  Copyright © 2019 Adam Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
