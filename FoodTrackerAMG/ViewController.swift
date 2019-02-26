f41dc7350a4aa46119bf40cb4e0cbf7a0bf43eb0
Adam Garcia <amg16h@lib-301-24-9308.local>
2019-02-25 18:59:24 -0600

    3rd Commit: Updated ViewController

--- FoodTrackerAMG/ViewController.swift
+++ FoodTrackerAMG/ViewController.swift
@@ -40,6 +40,19 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         dismiss(animated: true, completion: nil)
     }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        // The info dictionary may contain multiple representations of the image. You want to use the original.
+        guard let selectedImage = info[.originalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        
+        // Set photoImageView to display the selected image.
+        photoImageView.image = selectedImage
+        
+        // Dismiss the picker.
+        dismiss(animated: true, completion: nil)
+    }
     
 // MARK Actions
     @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
