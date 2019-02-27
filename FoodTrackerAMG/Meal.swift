e5fc680cb066db1a054551d92f53557f1865e46e
Adam Garcia <amg16h@lib-301-24-9308.local>
2019-02-26 22:18:32 -0600

    Define Data Model Complete: !st commit- Food Tracker Swift completed, class definition may be defined wrong. Made changes to Meal.swift. Meal.swift still has errors

---
+++ FoodTrackerAMG/Meal.swift
@@ -0,0 +1,56 @@
+//
+//  Meal.swift
+//  FoodTrackerAMG
+//
+//  Created by Adam Garcia on 2/26/19.
+//  Copyright © 2019 Adam Garcia. All rights reserved.
+//
+
+import Foundation
+
+class Meal {
+    
+    //MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    //MARK: Initialization
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        
+        // The name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        // The rating must be between 0 and 5 inclusively
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+        
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+    }
+    
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        
+        // Initialization should fail if there is no name or if the rating is negative.
+        if name.isEmpty || rating < 0  {
+            return nil
+        }
+        
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+    }
+
+}
+
