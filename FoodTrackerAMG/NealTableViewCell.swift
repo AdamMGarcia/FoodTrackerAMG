01df0ab376b0843d7d85d12c8ea4f791f7b2d582
Adam Garcia <amg16h@lib-301-24-9308.local>
2019-02-27 20:31:25 -0600

    Create a table view: 1st commit - started table view, completed table and stack view. Got to checkpoint.

---
+++ FoodTrackerAMG/NealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NealTableViewCell.swift
+//  FoodTrackerAMG
+//
+//  Created by Adam Garcia on 2/27/19.
+//  Copyright © 2019 Adam Garcia. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
